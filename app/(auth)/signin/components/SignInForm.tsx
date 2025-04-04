b366f75ba293e2ec066bd6efa399d5115891bba5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-04 22:19:57 +0100

    add imports from react

--- app/(auth)/signin/components/SignInForm.tsx
+++ app/(auth)/signin/components/SignInForm.tsx
@@ -23,7 +23,7 @@ import { signInSchema } from "@/lib/zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
-import { useActionState, useEffect, useRef } from "react";
+import { FormEvent, useActionState, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
