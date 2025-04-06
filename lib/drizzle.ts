1a616635a36fa8fd0e594a22a599d7b54a1c1350
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-06 08:57:03 +0100

    modified the schema to allow for admin and permissions

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -11,6 +11,10 @@ export const user = pgTable("user", {
   image: text("image"),
   createdAt: timestamp("created_at").notNull(),
   updatedAt: timestamp("updated_at").notNull(),
+  role: text("role"),
+  banned: boolean("banned"),
+  banReason: text("ban_reason"),
+  banExpires: timestamp("ban_expires"),
 });
 
 export const session = pgTable("session", {
@@ -24,6 +28,7 @@ export const session = pgTable("session", {
   userId: text("user_id")
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
+  impersonatedBy: text("impersonated_by"),
 });
 
 export const account = pgTable("account", {
