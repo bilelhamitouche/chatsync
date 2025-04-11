77ee06516dfc162fcfa9e7d3dadafa267b23549d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-11 14:52:40 +0100

    added user info display

--- app/(chat)/chat/page.tsx
+++ app/(chat)/chat/page.tsx
@@ -1,11 +1,18 @@
+import { getSessionInfo } from "@/actions/auth";
 import SignOutButton from "@/components/SignOutButton";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 async function Chat() {
+  const session = await getSessionInfo();
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-full">
       <h1 className="text-xl font-bold">Chat page</h1>
-      <Link href="/settings/account">Settings</Link>
+      <p>{session?.user.name}</p>
+      <p>{session?.user.email}</p>
+      <Button variant="link" size="sm" asChild>
+        <Link href="/settings/account">Settings</Link>
+      </Button>
       <SignOutButton />
     </div>
   );
