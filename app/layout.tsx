a179c71566f308f4ad3c2887c95e4b4ebe1a74db
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-11 03:49:01 +0100

    changed the colors of toaster to light with rich colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={`${geistSans.className} h-full antialiased`}>
         {children}
-        <Toaster closeButton richColors position="top-center" />
+        <Toaster theme="light" closeButton richColors position="top-center" />
       </body>
     </html>
   );
