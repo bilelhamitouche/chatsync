132dcdfb2e31b8a928fb29d31e49011a3f810a02
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-11 13:00:43 +0100

    added zod schemas for settings

---
+++ app/(settings)/settings/lib/zod.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const passwordChangeSchema = z.object({
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  confirm_new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+});
+
+export const accountChangeSchema = z.object({
+  name: z.string().trim(),
+  email: z.string().trim().email({ message: "Must be a valid Email address" }),
+});
