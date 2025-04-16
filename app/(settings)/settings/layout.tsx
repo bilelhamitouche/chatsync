9fc746bc83cf10a2f347ba17f48c914158b4aa1d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-16 16:18:57 +0100

    added a back to /chat link

--- app/(settings)/settings/layout.tsx
+++ app/(settings)/settings/layout.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
 import SettingsTabs from "./components/SettingsTabs";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container justify-center py-8 mx-auto space-y-4 max-w-3xl h-full">
+      <Button variant="link" className="text-gray-500" asChild>
+        <Link href="/chat">
+          <ArrowLeft />
+          <span>Back to Chat</span>
+        </Link>
+      </Button>
       <h1 className="text-2xl font-bold">Settings</h1>
       <SettingsTabs>{children}</SettingsTabs>
     </div>
