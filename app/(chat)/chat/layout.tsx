98619eb5ab49cc22b212344b313a2917bcd544b3
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-01 13:56:19 +0100

    added layout that wraps chat and individual chats

--- app/(chat)/chat/layout.tsx
+++ app/(chat)/chat/layout.tsx
@@ -1,5 +1,15 @@
+import AppSidebar from "@/components/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="w-full h-full">{children}</div>
+      </SidebarProvider>
+    </div>
+  );
 }
 
 export default Layout;
