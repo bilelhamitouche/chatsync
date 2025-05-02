dbed12936e32e89698a57e89e90c71c9599b2ca2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-02 17:50:05 +0100

    put query client provider on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import ClientProvider from "@/components/QueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${geistSans.className} h-full antialiased`}>
-        {children}
-        <Toaster theme="light" closeButton richColors position="top-center" />
-      </body>
+      <ClientProvider>
+        <body className={`${geistSans.className} h-full antialiased`}>
+          {children}
+          <Toaster theme="light" closeButton richColors position="top-center" />
+        </body>
+      </ClientProvider>
     </html>
   );
 }
