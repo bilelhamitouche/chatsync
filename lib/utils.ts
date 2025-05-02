392c05aa545946e832100a7a108f3208dddc71d6
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-02 17:49:06 +0100

    added some utility functions to fetch data on client

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function getChats() {
+  const response = await fetch("/api/chats");
+  const chats = await response.json();
+  return chats;
+}
+
+export async function getUsers() {
+  const response = await fetch("/api/users");
+  const users = await response.json();
+  return users;
+}
