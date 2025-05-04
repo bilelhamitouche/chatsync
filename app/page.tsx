f5c4bd1d6da34a40468f60c28209ecfd477f41e3
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-04 16:49:10 +0100

    added redirect to /chat if user is authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ import GetStarted from "@/components/GetStarted";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Testimonials from "@/components/Testimonials";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) redirect("/chat");
   return (
     <div>
       <Navbar />
