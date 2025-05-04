24d5e35346fbacb0a90f44994a262d682fa0956b
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-04 16:48:27 +0100

    fixed props, added a few styles, fixed typ̂es

--- app/(chat)/chat/components/navbar.tsx
+++ app/(chat)/chat/components/navbar.tsx
@@ -2,7 +2,11 @@ import CustomSidebarTrigger from "@/components/custom-sidebar-trigger";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface NavbarProps {
-  memberInfo: { imageUrl: string; name: string }[];
+  memberInfo: {
+    id: string | null;
+    name: string | null;
+    imageUrl: string | null;
+  }[];
 }
 
 function Navbar({ memberInfo }: NavbarProps) {
@@ -10,29 +14,32 @@ function Navbar({ memberInfo }: NavbarProps) {
     <header className="flex p-4 w-full shadow-sm shadow-b">
       <CustomSidebarTrigger />
       {memberInfo.length === 1 ? (
-        <div className="flex">
+        <div className="flex gap-2 items-center px-2" key={memberInfo[0].id}>
           <Avatar>
             <AvatarImage
-              src={memberInfo[0].imageUrl}
+              src={memberInfo[0].imageUrl as string}
               alt={`${memberInfo[0].name} image`}
             />
-            <AvatarFallback>{memberInfo[0].name.toUpperCase()}</AvatarFallback>
+            <AvatarFallback>
+              {memberInfo[0].name?.toUpperCase()[0]}
+            </AvatarFallback>
           </Avatar>
+          <span>{memberInfo[0].name}</span>
         </div>
       ) : (
         <div className="flex flex-col">
-          <ul className="flex">
-            {memberInfo.map((member, index) => (
-              <Avatar key={index}>
+          <ul className="flex gap-2 items-center">
+            {memberInfo.map((member) => (
+              <Avatar key={member.id}>
                 <AvatarImage
-                  src={member.imageUrl}
+                  src={member.imageUrl as string}
                   alt={`${member.name} image`}
                 />
                 <AvatarFallback>{member.name}</AvatarFallback>
               </Avatar>
             ))}
           </ul>
-          <p className="text-sm text-gray-500">{memberInfo.length} members</p>
+          <p className="text-sm text-gray-500">{memberInfo.length} Members</p>
         </div>
       )}
     </header>
