c158062cba22d27ea0aaab9b33d1b6ff5bbd98fd
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-07 20:02:58 +0100

    added pusher functionality to create messages and send them in realtime

--- actions/chat.ts
+++ actions/chat.ts
@@ -1,7 +1,8 @@
 "use server";
 
-import { createChat } from "@/lib/queries";
+import { createChat, createMessage } from "@/lib/queries";
 import { getUserInfo } from "./auth";
+import Pusher from "pusher";
 
 export async function createChatAction(formData: FormData) {
   const name = formData.get("name") as string;
@@ -16,5 +17,23 @@ export async function createChatAction(formData: FormData) {
 
 export async function createMessageAction(formData: FormData) {
   const newMessage = formData.get("message") as string;
+  const chat = formData.get("chatId") as string;
   const user = await getUserInfo();
+  const currentMessage = await createMessage(
+    newMessage,
+    user?.id as string,
+    chat,
+  );
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: "eu",
+    useTLS: true,
+  });
+  await pusher.trigger(chat, "chat", {
+    ...currentMessage,
+    senderName: user?.name,
+    senderImage: user?.image,
+  });
 }
