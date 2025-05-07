1e3b982fb69220d8b7cac8f4c72e0eb79830349f
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-07 20:05:21 +0100

    added prop to take chatId param

--- app/(chat)/chat/components/ChatForm.tsx
+++ app/(chat)/chat/components/ChatForm.tsx
@@ -2,13 +2,14 @@ import { createMessageAction } from "@/actions/chat";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-export default function ChatForm() {
+export default async function ChatForm({ chatId }: { chatId: string }) {
   return (
     <form
       action={createMessageAction}
       className="flex gap-2 items-center p-2 w-full"
     >
-      <Input placeholder="Enter message" />
+      <Input placeholder="Enter message" name="message" />
+      <Input type="hidden" name="chatId" value={chatId} />
       <Button type="submit">Send Message</Button>
     </form>
   );
