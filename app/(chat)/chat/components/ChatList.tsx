9ac57727d601ceaee57cebcdaa17c080a8c41dee
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-25 19:36:35 +0100

    added skeleton loading and made fetching parallel

--- app/(chat)/chat/components/ChatList.tsx
+++ app/(chat)/chat/components/ChatList.tsx
@@ -4,61 +4,38 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Skeleton } from "@/components/ui/skeleton";
-import { authClient } from "@/lib/auth-client";
 import { Chat } from "@/lib/types";
-import { getChats } from "@/lib/utils";
-import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { toast } from "sonner";
 
-export default function ChatList() {
-  const { data: session } = authClient.useSession();
-  const isAuthenticated = session !== null;
-  const {
-    data: userChats,
-    isPending,
-    isError,
-  } = useQuery({
-    queryKey: ["chats"],
-    queryFn: getChats,
-    enabled: isAuthenticated,
-  });
-  if (isError) toast.error("Cannot fetch chats");
-  if (userChats?.chats != null)
-    return (
-      <SidebarMenu>
-        {userChats.chats.map((chat: Chat) => {
-          if (chat != null) {
-            return (
-              <SidebarMenuItem key={chat?.id}>
-                <SidebarMenuButton asChild>
-                  <Link href={`/chat/${chat.id}`}>
-                    {isPending ? (
-                      <div className="flex gap-2 items-center">
-                        <Skeleton className="rounded-full size-8" />
-                        <Skeleton className="h-6 w-[200px]" />
-                      </div>
-                    ) : (
-                      <div className="flex gap-2 items-center">
-                        <Avatar>
-                          <AvatarImage
-                            src={chat.userImage as string}
-                            alt={`${chat.userName} image`}
-                          />
-                          <AvatarFallback>
-                            {chat.userName[0].toUpperCase()}
-                          </AvatarFallback>
-                        </Avatar>
-                        <span>{chat.name}</span>
-                      </div>
-                    )}
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            );
-          }
-        })}
-      </SidebarMenu>
-    );
+interface ChatListProps {
+  chats: Chat[];
+}
+
+export default function ChatList({ chats }: ChatListProps) {
+  return (
+    <SidebarMenu>
+      {chats.map((chat: Chat) => {
+        return (
+          <SidebarMenuItem key={chat?.id}>
+            <SidebarMenuButton asChild>
+              <Link href={`/chat/${chat.id}`} className="w-full">
+                <div className="flex gap-2 items-center w-full">
+                  <Avatar>
+                    <AvatarImage
+                      src={chat.userImage as string}
+                      alt={`${chat.userName} image`}
+                    />
+                    <AvatarFallback>
+                      {chat.userName[0].toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span>{chat.name}</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        );
+      })}
+    </SidebarMenu>
+  );
 }
