673fb1a17c18c73a85bc8e7f7abda8b3dcc9a162
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-25 05:54:24 +0100

    feat: add separator for days

--- app/(chat)/chat/components/Messages.tsx
+++ app/(chat)/chat/components/Messages.tsx
@@ -7,6 +7,7 @@ import { useEffect, useRef, useState } from "react";
 import Pusher from "pusher-js";
 import { ChatMessage } from "@/lib/types";
 import ChatBubble from "./ChatBubble";
+import { Separator } from "@/components/ui/separator";
 
 export default function Messages({
   initialMessages,
@@ -37,29 +38,41 @@ export default function Messages({
     <ScrollArea className="p-4 w-full h-full">
       <div className="flex flex-col gap-4 w-full h-full">
         {messages &&
-          messages.map((message: ChatMessage) => (
-            <li
-              className={`flex items-center max-w-full ${message.senderId === session?.user.id ? "justify-end" : "justify-start"}`}
-              key={message.id}
-            >
-              <div
-                className={`flex ${message.senderId === session?.user.id ? "flex-row-reverse" : "flex-row"} gap-2 items-start max-w-full`}
+          messages.map((message: ChatMessage, index) => (
+            <div key={message.id}>
+              {index > 0 &&
+              new Date(messages[index].createdAt).getDate() -
+                new Date(messages[index - 1].createdAt).getDate() ===
+                1 ? (
+                <div className="flex relative items-center py-4">
+                  <Separator />
+                  <span className="absolute left-1/2 px-2 m-2 mx-auto text-sm text-gray-500 -translate-x-1/2 bg-background">
+                    {new Date().toLocaleDateString()}
+                  </span>
+                </div>
+              ) : null}
+              <li
+                className={`flex items-center max-w-full ${message.senderId === session?.user.id ? "justify-end" : "justify-start"}`}
               >
-                <Avatar>
-                  <AvatarImage
-                    src={message.senderImage as string}
-                    alt={`${message.senderName} image`}
+                <div
+                  className={`flex ${message.senderId === session?.user.id ? "flex-row-reverse" : "flex-row"} gap-2 items-start max-w-full`}
+                >
+                  <Avatar>
+                    <AvatarImage
+                      src={message.senderImage as string}
+                      alt={`${message.senderName} image`}
+                    />
+                    <AvatarFallback>
+                      {message.senderName?.toUpperCase()[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                  <ChatBubble
+                    content={message.content}
+                    createdAt={message.createdAt}
                   />
-                  <AvatarFallback>
-                    {message.senderName?.toUpperCase()[0]}
-                  </AvatarFallback>
-                </Avatar>
-                <ChatBubble
-                  content={message.content}
-                  createdAt={message.createdAt}
-                />
-              </div>
-            </li>
+                </div>
+              </li>
+            </div>
           ))}
       </div>
       <div ref={scrollRef}></div>
