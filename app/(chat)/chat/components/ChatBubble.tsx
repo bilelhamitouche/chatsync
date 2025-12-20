b310bf51728298f666ed8f3d88b2550eb810407f
Bilel Hamitouche <bilelhamitouche4@gmail>
2025-12-20 16:13:54 +0100

    styles: make chat bubble fit content

--- app/(chat)/chat/components/ChatBubble.tsx
+++ app/(chat)/chat/components/ChatBubble.tsx
@@ -8,8 +8,8 @@ interface ChatBubbleProps {
 
 export default function ChatBubble({ content, createdAt }: ChatBubbleProps) {
   return (
-    <div className="flex flex-col gap-1 min-w-0 max-w-full">
-      <div className="p-2 min-w-0 max-w-xs text-white whitespace-pre-wrap break-words rounded-lg sm:max-w-xl bg-primary text-wrap">
+    <div className="flex flex-col gap-1 max-w-full">
+      <div className="self-end p-2 max-w-xs text-white whitespace-pre-wrap break-words rounded-lg sm:max-w-xl bg-primary text-wrap">
         {content}
       </div>
       <span className="pl-4 text-xs text-left text-gray-500">
