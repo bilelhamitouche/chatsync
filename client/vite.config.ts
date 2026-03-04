5a385e517b6b11ec60d91c3d338c97f58608002a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-04 02:38:26 +0100

    change vite port to 3000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     tanstackRouter({
       target: "react",
