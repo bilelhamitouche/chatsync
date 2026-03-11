302aa601209cf4c57e23fb07ae7361b324fa59ac
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-11 13:30:19 +0100

    feat: add daisyui

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
