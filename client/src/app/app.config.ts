2b064368c0b0b689b67ab9c9e3b7d686f13c72a4
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-12 03:20:49 +0100

    chore: add primeng ui library

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeuix/themes/material';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+      },
+    }),
+  ],
 };
