00e62787715d3f0419bf6209dfcaf7dcd6f927cf
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-20 18:49:43 +0100

    feat: add util functions for fetch and name formatting

---
+++ client/src/utils/formatAvatarName.ts
@@ -0,0 +1,8 @@
+export function formatAvatarName(name: string) {
+  const nameArray = name.split(" ");
+  if (nameArray.length === 1) {
+    return nameArray[0][0].toUpperCase();
+  }
+  const newArray = nameArray.map((item) => item[0]);
+  return (newArray[0] + newArray[newArray.length - 1]).toUpperCase();
+}
