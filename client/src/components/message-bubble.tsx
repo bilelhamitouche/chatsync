de365a11e30128bba3d523b4562ed88d4e48b775
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-31 04:50:05 +0100

    refactor: change message type prop

--- client/src/components/message-bubble.tsx
+++ client/src/components/message-bubble.tsx
@@ -1,17 +1,8 @@
+import type { Message } from "@/lib/types";
 import { Box, Image, Text } from "@chakra-ui/react";
 
 interface MessageBubbleProps {
-  message: {
-    id: string;
-    content: string | null;
-    imageUrl: string | null;
-    senderId: string;
-    senderName: string;
-    senderAvatar: string;
-    chatId: string;
-    sentAt: Date;
-    updatedAt: Date;
-  };
+  message: Message;
   currentUserId: string;
 }
 
@@ -21,28 +12,37 @@ export default function MessageBubble({
 }: MessageBubbleProps) {
   return (
     <Box
-      key={message.id}
-      borderRadius="md"
-      px="4"
-      py="3"
-      w="fit-content"
       alignSelf={message.senderId === currentUserId ? "flex-end" : "flex-start"}
-      bg={message.senderId === currentUserId ? "blue.solid" : "gray.200"}
-      color={message.senderId === currentUserId ? "bg" : "fg"}
     >
-      <Box spaceY="4">
-        {message.imageUrl ? (
-          <Image
-            src={message.imageUrl as string}
-            alt="image content"
-            maxW="300px"
-            maxH="400px"
-            objectFit="contain"
-            borderRadius="md"
-          />
-        ) : null}
-        {message.content ? <Text>{message.content}</Text> : null}
+      <Box
+        borderRadius="md"
+        px="4"
+        py="3"
+        w="fit-content"
+        maxW="md"
+        textWrap="wrap"
+        textWrapMode="wrap"
+        textWrapStyle="pretty"
+        bg={message.senderId === currentUserId ? "blue.solid" : "gray.200"}
+        color={message.senderId === currentUserId ? "bg" : "fg"}
+      >
+        <Box spaceY="4">
+          {message.imageUrl ? (
+            <Image
+              src={message.imageUrl as string}
+              alt="image content"
+              maxW="300px"
+              maxH="400px"
+              objectFit="contain"
+              borderRadius="md"
+            />
+          ) : null}
+          {message.content ? <Text>{message.content}</Text> : null}
+        </Box>
       </Box>
+      <Text fontSize="xs" color="gray.600">
+        {new Date(message.sentAt).toLocaleDateString()}
+      </Text>
     </Box>
   );
 }
