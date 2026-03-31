2121dfa28a436423fe4f27feff56b0a3034f483b
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-31 05:00:20 +0100

    fix: type and delete unused error handling

--- client/src/api/mutations/messages.ts
+++ client/src/api/mutations/messages.ts
@@ -1,17 +1,14 @@
-import type { createMessageData } from "@/lib/types";
+import type { CreateMessageData } from "@/lib/types";
 import { apiFetch } from "@/utils/apiFetch";
 import { useMutation } from "@tanstack/react-query";
 
 export const createMessageMutation = () =>
   useMutation({
-    mutationFn: async (data: createMessageData) => {
+    mutationFn: async (data: CreateMessageData) => {
       await apiFetch("/api/messages", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
       });
-    },
-    onError: (error) => {
-      throw new Error(error.message);
     },
   });
