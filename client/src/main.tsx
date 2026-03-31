95201e76af5cbfb772ff0f6cdc4bb07785e8e4f7
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-31 04:59:20 +0100

    feat: add toaster component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from "./components/ui/provider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import "@fontsource-variable/bricolage-grotesque/standard.css";
 import { queryClient, router } from "./lib/router";
+import { Toaster } from "./components/ui/toaster";
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <Provider>
         <RouterProvider router={router} />
+        <Toaster />
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
