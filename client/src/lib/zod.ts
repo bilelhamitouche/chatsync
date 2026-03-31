6b911a48d9f0d8c44db51b9616bbd18c45154ae8
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-03-31 04:50:36 +0100

    feat: add zod schemas for form validation

--- client/src/lib/zod.ts
+++ client/src/lib/zod.ts
@@ -36,3 +36,24 @@ export const createMessageSchema = z.object({
     .trim()
     .refine((value) => value.split(" ").length < 500),
 });
+
+export const updateProfileInformationSchema = z.object({
+  name: z.string().trim().min(1, { error: "Name is required" }),
+  email: z.email({ error: "Must be a valid email address" }).trim(),
+});
+
+export const updatePasswordSchema = z
+  .object({
+    currentPassword: z
+      .string()
+      .trim()
+      .min(8, { error: "Password should be at least 8 characters" }),
+    password: z
+      .string()
+      .trim()
+      .min(8, { error: "Password should be at least 8 characters" }),
+  })
+  .refine((data) => data.password !== data.currentPassword, {
+    error: "Password cannot be the same as current password",
+    path: ["password"],
+  });
