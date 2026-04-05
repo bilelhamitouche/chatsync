d6c7a357182ffcfd3d50c997c24426abf5368ef6
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-04-05 17:54:58 +0100

    feat: add reconnections for socket.io

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -3,4 +3,7 @@ import { io } from "socket.io-client";
 export const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
   autoConnect: false,
+  reconnection: true,
+  reconnectionDelay: 1000 * 60 * 14,
+  reconnectionAttempts: 5,
 });
